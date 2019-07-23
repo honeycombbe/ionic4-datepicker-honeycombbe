256633ffc4934c107b300286c8cef2e8c5dacf9f
Matt Nick <mnick@honeycomb.be>
2019-07-23 16:11:09 -0500

    update

---
+++ ionic4-calendar-picker-honeycombbe/ionic4-calendar-picker-honeycombbe.component.js
@@ -0,0 +1,12 @@
+class Ionic4CalendarPickerHoneycombbeController {
+    constructor() {
+    }
+}
+
+export default {
+    controller: Ionic4CalendarPickerHoneycombbeController,
+    template: require('./ionic4-calendar-picker-honeycombbe.html'),
+    bindings: {
+        
+    }
+};
