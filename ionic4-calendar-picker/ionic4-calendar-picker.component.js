82ac3d491820cdeeb6f47d4c378c38fd9bcea22f
Matt Nick <mnick@honeycomb.be>
2019-07-23 16:36:41 -0500

    udpate

---
+++ ionic4-calendar-picker/ionic4-calendar-picker.component.js
@@ -0,0 +1,12 @@
+class Ionic4CalendarPickerController {
+    constructor() {
+    }
+}
+
+export default {
+    controller: Ionic4CalendarPickerController,
+    template: require('./ionic4-calendar-picker.html'),
+    bindings: {
+        
+    }
+};
